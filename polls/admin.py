ee7c92971f8b954f138df052a60f100140d8574d
junpark <junpark@vcanus.com>
2022-01-08 23:02:49 +0900

    add Comment model

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Choice, Question
+from .models import Choice, Question, Comment
 
 admin.site.register(Choice)
 admin.site.register(Question)
+admin.site.register(Comment)
--- polls/models.py
+++ polls/models.py
@@ -7,7 +7,10 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
-    is_closed = models.BooleanField("is poll closed")
+    is_closed = models.BooleanField("is poll closed", default=False)
+
+    class Meta:
+        ordering = ['pub_date']
 
     def __str__(self):
         return self.question_text
@@ -23,3 +26,16 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+
+class Comment(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return 'Comment {} by {}'.format(self.body, self.name)
