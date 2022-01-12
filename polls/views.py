abf5278f0c79694a13bc1b148f07220b680312dd
Jun Park <junxcareer@gmail.com>
2022-01-12 22:41:35 +0900

    add comment to question form, post function

--- polls/views.py
+++ polls/views.py
@@ -3,12 +3,14 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.http import Http404
 from rest_framework import viewsets, filters
 import django_filters
 
 from utils.url import restify
 
-from .models import Choice, Question
+from .models import Choice, Question, Comment
+from .forms import CommentForm
 from .serializers import QuestionSerializer
 
 
@@ -32,6 +34,25 @@ class DetailView(generic.DetailView):
 class ResultsView(generic.DetailView):
     model = Question
     template_name = "polls/results.html"
+
+
+def comment(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current question to the comment
+            new_comment.question = question
+            # Save the comment to the database
+            new_comment.save()
+
+    else:
+        raise Http404('No valid access')
+
+    return HttpResponseRedirect(reverse("polls:detail", args=(question_id,)))
 
 
 def vote(request, question_id):
